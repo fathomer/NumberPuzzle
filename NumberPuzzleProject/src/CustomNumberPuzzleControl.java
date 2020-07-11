ac287304a9735b1908a209b8fd09a9d2dc8d9272
Kartikeya Manchanda <kartikabc712@gmail.com>
2020-07-11 16:19:20 +0530

    Updated with the complete solution

--- NumberPuzzleProject/src/CustomNumberPuzzleControl.java
+++ NumberPuzzleProject/src/CustomNumberPuzzleControl.java
@@ -33,45 +33,59 @@ class CustomNumberPuzzleControl extends NumberPuzzleControl {
 	// The following three methods have to be written by the participants...
 
 	public int handleButtonClicked(NumberPuzzleGame game){
+		
 		int emptyCellId = game.getEmptyCellId();
 		Button buttonClicked = game.getButtonClicked();
 		Button[] buttons = game.getButtons();
+		//Now we need the CellId of the button clicked
+		int clickCellId = 0;
+		for (int i = 0;i <16; i++) { //We traverse the buttons array to find at which index is the clicked button
+			if (buttons[i].getLabel()==buttonClicked.getLabel())
+				clickCellId = i;
+		}
 		
-		//Your logic here		
+		//Now we calculate the row and column for empty and clicked cell
+		int clickRow = clickCellId / 4, clickCol = clickCellId % 4;
+		int emptyRow = emptyCellId / 4, emptyCol = emptyCellId % 4;
+		int rowDiff = Math.abs(clickRow-emptyRow);
+		int colDiff = Math.abs(clickCol-emptyCol);
 		
-		return emptyCellId;
+		//If the clicked cell is not adjacent or is on diagonal to empty cell then do nothing and return older emptycell value
+		if (rowDiff > 1 || colDiff > 1 || (rowDiff > 0 && colDiff > 0)) 
+			return emptyCellId;
+		
+		//Else if
+		swapButton(buttons[emptyCellId],buttonClicked);
+		System.out.println();
+		emptyCellId = clickCellId;
+		return clickCellId;
 
 	}
 	public int[] getRandomNumbersForGrid() {
 		int arr[] = new int[15];
 		boolean flag = false;
 		
-		for(int i = 0; i < 15;) {
+		for(int i = 0; i < 15;) { //We need to generate 15 new numbers
 			flag = false;
-			System.out.print(i + "I \n");
-			int a = getRandomNumber();
-			System.out.print(a + "Non random A \n");
-			a = (14 * a /99) + 1;
-			System.out.print(a + "Random A \n");
+			int a = getRandomNumber(); //This function generates numbers from 1-100
+			a = (14 * a /99) + 1; //This will limit the generated number to 1-15
 			for (int index = i;index >=0;index--) { 
-				if (arr[index] == a) { 
+				if (arr[index] == a) { //Checking if the generated number is there in the array earlier or not
 					flag = true; 
 			        break; 
 				}
 			}
-			if (flag) continue;
-			arr[i++] = a;
+			if (flag) continue; //If number already there proceed again without incrementing
+			arr[i++] = a; //Else increment and update array
 		}
 				
 		return arr;
 	}
+	
 	public boolean checkForWinner(Button[] buttons)
 	{
-		boolean winner = true;
-		
-		// Your Logic here
-		getIntegerArrayOfButtonIds(buttons);
-
-		return winner;
+		for (int i = 0;i <15;i++)//We traverse the buttons array to find at which index is the clicked button
+			if(buttons[i].getLabel()=="  " || Integer.parseInt(buttons[i].getLabel())!=i+1) return false;
+		return true;
 	}
 }
